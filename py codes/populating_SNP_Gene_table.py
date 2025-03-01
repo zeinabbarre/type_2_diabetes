0165208566ebcc86e9109eea8dad477473578111
zeinabbarre <zeinabbarreuni@gmail.com>
2025-03-01 14:37:16 +0000

    Update populating_SNP_Gene_table.py

--- py codes/populating_SNP_Gene_table.py
+++ py codes/populating_SNP_Gene_table.py
@@ -1,13 +1,9 @@
-import os
 import pandas as pd
 import sqlite3
 
-# Get the base directory of the script
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-# Use relative paths for files
-csv_file_path = os.path.join(BASE_DIR, "csv_files", "snp_gene.csv")
-db_path = os.path.join(BASE_DIR, "instance", "db.db")
+# File paths
+csv_file_path = "/Users/zeinabbarre/Desktop/type_2_diabetes/csv_files/snp_gene.csv"
+db_path = "/Users/zeinabbarre/Desktop/type_2_diabetes/instance/db.db"
 
 # Load CSV file
 df = pd.read_csv(csv_file_path)
@@ -39,4 +35,3 @@ cursor.close()
 conn.close()
 
 print("✅ SNP-Gene mappings inserted successfully!")
-
