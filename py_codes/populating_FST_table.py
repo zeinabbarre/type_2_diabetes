5f400e47ce9ac4b034d712079278684959a838ba
stargirlmaz <maryibitoye23@gmail.com>
2025-03-06 17:01:30 +0000

    Update populating_FST_table.py

--- py_codes/populating_FST_table.py
+++ py_codes/populating_FST_table.py
@@ -1,35 +1,5 @@
 import sqlite3
 import pandas as pd
-
-# File paths
-db_path = "/Users/yasminahmed/Desktop/type_2_diabetes/instance/db.db"
-csv_path = "/Users/yasminahmed/Desktop/type_2_diabetes/csv_files/fst_values_cleaned.csv"
-
-# Load the cleaned CSV
-df = pd.read_csv(csv_path)
-
-# Connect to SQLite
-conn = sqlite3.connect(db_path)
-cursor = conn.cursor()
-
-# Insert data while ensuring no NULL values are inserted
-for index, row in df.iterrows():
-    cursor.execute("""
-        INSERT INTO Fst_Values (chromosome, position, fst_value, population_1, population_2)
-        VALUES (?, ?, ?, ?, ?)
-    """, (row["CHROM"], row["POS"], row["WEIR_AND_COCKERHAM_FST"], row["population_1"], row["population_2"]))
-
-# Commit changes and close
-conn.commit()
-conn.close()
-
-print("✅ Data inserted successfully!")
-
-
-
-
-import sqlite3
-import pandas as pd
 import os
 
 # ✅ Define Base Directory (moves up one level to reach the root project folder)
@@ -56,4 +26,3 @@ conn.commit()
 conn.close()
 
 print("✅ Data inserted successfully!")
-
